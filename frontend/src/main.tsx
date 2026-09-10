9e0bf7615da3a1b1c35495b721ba684c1c7c0968
Daniel Zheng <daniel.zheng923125@gmail.com>
2026-09-10 18:07:57 -0400

    added vercel web analytics

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Analytics } from "@vercel/analytics/react"
 import './index.css'
 import App from './App.tsx'
 import UnlimitedPage from './UnlimitedPage.tsx'
@@ -14,6 +15,7 @@ createRoot(document.getElementById('root')!).render(
         <Route path="/unlimited" element={<UnlimitedPage />} />
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
+      <Analytics />
     </BrowserRouter>
   </StrictMode>,
 )
