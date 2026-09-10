cedf054df07e924cb445d23aacbcfc19a043395b
Daniel Zheng <daniel.zheng923125@gmail.com>
2026-09-10 18:12:29 -0400

    added vercel web analytics

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -15,7 +15,7 @@ createRoot(document.getElementById('root')!).render(
         <Route path="/unlimited" element={<UnlimitedPage />} />
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
-      <Analytics />
     </BrowserRouter>
+    <Analytics />
   </StrictMode>,
 )
